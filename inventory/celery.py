701521203e8ecd19b43283e8e45cc445365249db
Smit Panchal <smitpanchal32@gmail.com>
2023-04-26 22:23:06 +0530

    - Implemeny celery.

---
+++ inventory/celery.py
@@ -0,0 +1,35 @@
+# Standard imports
+from __future__ import absolute_import, unicode_literals
+import os
+
+# 3rd party imports
+from celery import Celery
+from django.conf import settings
+from celery.schedules import crontab
+
+# Setting up default environment
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'inventory.settings')
+
+app = Celery('inventory')
+app.conf.enable_utc = False
+
+app.conf.update(timezone='Asia/Kolkata')
+
+app.config_from_object(settings, namespace='CELERY')
+
+# Celery Beat Settings
+# app.conf.beat_schedule = {
+#     'send-mail-every-day-at-8': {
+#         'task': 'send_mail_app.task.send_mail_func',
+#         'schedule': crontab(hour=19, minute=15),
+#         # 'args': (2, )
+#     }
+# }
+
+# TO auto-discover all apps tasks
+app.autodiscover_tasks()
+
+
+@app.task(bind=True)
+def debug_task(self):
+    print(f"Request: {self.request!r}")
